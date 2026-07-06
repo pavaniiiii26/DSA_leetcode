3e7223cfa23d17b1ee5aa90b541c6bbbf1b66246
Pavani Patel <pavanigpatel@gmail.com>
2026-07-07 01:16:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (4.03%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.c
@@ -0,0 +1,17 @@
+char* triangleType(int* nums, int numsSize) {
+
+if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) {
+
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        }
+
+        if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]) {
+            return "scalene";
+        }
+
+        return "isosceles";
+    }
+
+    return "none";
+}
