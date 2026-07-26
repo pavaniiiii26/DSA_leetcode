18c95358e9961fbc964dc2511075f7bfa175fef8
Pavani Patel <pavanigpatel@gmail.com>
2026-07-26 14:17:20 +0530

    Time: 22 ms (25.79%) | Memory: 11.1 MB (14.68%) - LeetSync

---
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.c
@@ -0,0 +1,12 @@
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int maximumProduct(int* nums, int numsSize) {
+qsort(nums, numsSize, sizeof(int), compare);
+
+int product1= nums[numsSize-1]* nums[numsSize-2]* nums[numsSize-3];
+int product2= nums[0] * nums[1] * nums[numsSize - 1];
+
+return (product1 > product2) ? product1 : product2;
+}  
